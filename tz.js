01b0e2a551a788da35b653e8a3062933a0f56a74
David DiBenedetto <david@njweb.solutions>
2022-10-17 13:23:24 -0400

    fix no argument crash
    Print error message with instructions when no first argument is passed

--- tz.js
+++ tz.js
@@ -1,6 +1,11 @@
 const moment = require('moment-timezone');
+const { exit } = require('yargs');
 const yargs = require('yargs');
 
+if (yargs.argv._[0] === undefined) {
+  console.log('enter timezone as argument 1');
+  exit(1);
+}
 moment.tz.setDefault('America/New_York');
 if (yargs.argv.format) {
   console.log(moment().tz(yargs.argv._[0]).format('dddd, MMMM Do YYYY, h:mm:ss a'));
